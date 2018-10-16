0643956431e652e6e8a3bfb2db0d9e909d759f40
sdk1270 <sdk1270@gmail.com>
2018-10-16 20:38:22 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,6 @@ def speak_to_grandma = (string)
 
   if string != string.upcase
    puts "HUH?! SPEAK UP, SONNY!"
-   puts "NO, NOT SINCE 1938!" 
   elsif speak_to_grandma == "I LOVE YOU GRANDMA!"
    puts "I LOVE YOU TOO PUMPKIN!"
   else 
